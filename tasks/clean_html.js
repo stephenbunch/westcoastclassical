301c62c0fa144d3190698fe0f95d5b35a15d63e8
Stephen Bunch <bunchclone@gmail.com>
2015-08-05 19:51:37 -0700

    fix clean task

--- tasks/clean_html.js
+++ tasks/clean_html.js
@@ -2,10 +2,14 @@ import del from 'del';
 
 export default function( projectDir, done ) {
   del([
-    `${ projectDir }/www/**/*.html`,
+    `${ projectDir }/www/**`,
+    `!${ projectDir }/www`,
     `!${ projectDir }/www/site.css`,
     `!${ projectDir }/www/site.js`,
-    `!${ projectDir }/img/**/*`,
-    `!${ projectDir }/favicon.ico`
+    `!${ projectDir }/www/img`,
+    `!${ projectDir }/www/img/**/*`,
+    `!${ projectDir }/www/fonts`,
+    `!${ projectDir }/www/fonts/**/*`,
+    `!${ projectDir }/www/favicon.ico`
   ], done );
 };
